30a2b01899e980d5484d3f8dc78ff82e2900c8a0
fangsinan <15650701817@163.com>
2020-10-30 16:35:54 +0800

    no message

--- App/HttpController/V1/Push.php
+++ App/HttpController/V1/Push.php
@@ -136,7 +136,6 @@ class Push extends Controller
 
                 $LiveComment=new LiveCommentModel();
                 $LiveComment->add(LiveCommentModel::$table,['live_id'=>$live_id,'user_id'=>$user_id,'content'=>$content,'created_at'=>date('Y-m-d H:i:s',time())]);
-                echo $LiveComment->getLastQuery();
             });
 
         }else{
