4c802ebc0c6d88949204a7a5149ff8f37b7187f2
fangsinan <15650701817@163.com>
2020-12-02 15:15:34 +0800

    no message

--- App/HttpController/V1/Push.php
+++ App/HttpController/V1/Push.php
@@ -183,7 +183,7 @@ class Push extends Controller
             //$content=json_encode(['giftChoose'=>$gift_class,'giftNumber'=>$gift_num,'gift_price'=>$gift_price,'nickname' => $UserInfo['result']['nickname']]);
             $content=['giftChoose'=>$gift_class,'giftNumber'=>$gift_num,'gift_price'=>$gift_price,'nickname' => $UserInfo['result']['nickname']];
 
-            $data = json_encode(['type' => 12, 'content' => $content,'content_obj' => $content,
+            $data = json_encode(['type' => 12, 'ios_content' => $content,'content_obj' => $content,
                 'userinfo' => ['level' => $UserInfo['result']['level'], 'nickname' => $UserInfo['result']['nickname'],'user_id'=>$user_id]]);
 
             $live_gift=Config::getInstance()->getConf('web.live_gift');
