7ca34ed1c144bf94326bdf7dca6d81d595c3a5ec
fangsinan <15650701817@163.com>
2021-01-16 15:01:58 +0800

    no message

--- App/Services/V1/PushService.php
+++ App/Services/V1/PushService.php
@@ -84,15 +84,13 @@ class PushService
             $url = "http://$val:9581/index/broadcast";
 //            $info = self::CurlPost($url,['live_id'=>$live_id,'data'=>$data_str]);
 
-
             for ($i = 0; $i <= 3 ;$i++){
                 $info = self::CurlPost($url,['live_id'=>$live_id,'data'=>$data_str]);
                 $res = json_decode($info,true);
-                var_dump($res['msg'] == 1);
                 if($res['msg'] == 1){
-                    echo 'for  for';
                     break;
                 }
+                echo 'for  for';
             }
 
 
