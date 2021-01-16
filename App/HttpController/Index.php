67576fcacc0d08efac2b4a9099ee4cd29fd9b690
fangsinan <15650701817@163.com>
2021-01-16 13:45:40 +0800

    no message

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -83,8 +83,6 @@ class Index extends  Controller
         $params  = $this->request()->getRequestParam();
         $ListPort = swoole_get_local_ip(); //获取监听ip
 
-        print_r('params');
-        print_r($params);
         $PushService=new PushService();
         $rst=$PushService->Broadcast($ListPort['eth0'],$params);
 
