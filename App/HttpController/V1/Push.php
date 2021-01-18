3d4dabd1d64bf90e27571feeb180900331ec9497
fangsinan <15650701817@163.com>
2021-01-18 14:44:53 +0800

    no message

--- App/HttpController/V1/Push.php
+++ App/HttpController/V1/Push.php
@@ -134,7 +134,7 @@ class Push extends Controller
 
             $content = Common::textEncode($UserInfo['result']['content']); //入库内容信息 处理表情
 //            $content = $UserInfo['result']['content'];
-
+echo $content.'\n';
 //            $data = json_encode(['type' => 2, 'content' =>Common::textDecode($content),'content_text'=>Common::textDecode($content), 'userinfo' => ['user_id'=>$message['user_id'],
 //                    'level' => $UserInfo['result']['level'],'nickname' => $UserInfo['result']['nickname']]]);
             $data = json_encode(['type' => 2, 'content_text'=>Common::textDecode($content), 'userinfo' => ['user_id'=>$message['user_id'],
