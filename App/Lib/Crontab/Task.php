ee12cc79dc589c027f637baf4e06e96604da92bf
fangsinan <15650701817@163.com>
2021-03-15 16:58:24 +0800

    no message

--- App/Lib/Crontab/Task.php
+++ App/Lib/Crontab/Task.php
@@ -437,8 +437,11 @@ class Task extends \EasySwoole\EasySwoole\Swoole\Task\AbstractAsyncTask
                     'img'=>'/nlsg/works/20201124144228445465.png'
                 ];
             }else if($val['push_type'] == 9){
-                $fields = 'id, title, describe, cover_img,begin_at, end_at, user_id, price, is_free';
+                $fields = 'id, title name, describe subtitle, cover_img img,cover_img image,begin_at, end_at, user_id, price, is_free';
                 $Info = $liveObj->getOne($liveObj->tableName,['id'=>$val['push_gid'],'status'=>4,'is_del'=>0,'is_test'=>0],$fields);
+
+                $live_Info = $liveObj->getOne("nlsg_live_info",['live_pid'=>$val['push_gid']],"id");
+                $Info['live_info_id'] = $live_Info['id'];
             }
             if($Info){
                 $res[]= [
