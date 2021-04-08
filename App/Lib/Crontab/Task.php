131834cad9af79a976ae5b0556571e6e889365fa
fangsinan <15650701817@163.com>
2021-04-08 13:54:00 +0800

    no message

--- App/Lib/Crontab/Task.php
+++ App/Lib/Crontab/Task.php
@@ -326,6 +326,7 @@ class Task extends \EasySwoole\EasySwoole\Swoole\Task\AbstractAsyncTask
             foreach($listRst as $key => $val){
                 $arr = explode ('_', $val);
                 $live_id=$arr[2];
+                //$noticeList = $noticeObj->get($noticeObj->tableName,['live_info_id'=>$live_id,'is_send'=>1,'is_del'=>0,'is_done'=>0],'id,live_id,live_info_id,content,length,created_at,type,content_type');
                 $noticeList = $noticeObj->get($noticeObj->tableName,['live_info_id'=>$live_id,'is_send'=>1,'is_done'=>0],'id,live_id,live_info_id,content,length,created_at,type,content_type,is_del');
                 if(!empty($noticeList)){
                     $data = Common::ReturnJson (Status::CODE_OK,'发送成功',['type' => 7,'ios_content' =>$noticeList[0], 'content_obj' =>$noticeList[0] ]);
