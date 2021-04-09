3a0840ced8361f900611650e992feadbb29d42c7
fangsinan <15650701817@163.com>
2021-04-09 14:12:23 +0800

    no message

--- App/Lib/Crontab/Task.php
+++ App/Lib/Crontab/Task.php
@@ -328,7 +328,7 @@ class Task extends \EasySwoole\EasySwoole\Swoole\Task\AbstractAsyncTask
                 $live_id=$arr[2];
                 $noticeList = $noticeObj->get($noticeObj->tableName,['live_info_id'=>$live_id,'type'=>1, 'is_send'=>1,'is_del'=>0,'is_done'=>0],'id,live_id,live_info_id,content,length,created_at,type,content_type');
                 if(!empty($noticeList)){
-                    $data = Common::ReturnJson (Status::CODE_OK,'发送成功',['type' => 7,'ios_content' =>$noticeList[0], 'content' =>$noticeList[0],'content_obj' =>$noticeList[0]]);
+                    $data = Common::ReturnJson (Status::CODE_OK,'发送成功',['type' => 7,'ios_content' =>$noticeList[0], 'content_obj' =>$noticeList[0]]);
                     //修改标记
                     $idArr=array_column($noticeList, 'id');
                     $noticeObj->update($noticeObj->tableName,['is_done'=>1,'done_at'=>date('Y-m-d H:i:s',time())],['id'=>$idArr]);
