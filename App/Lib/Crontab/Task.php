b24232a66a5f34b740279b6737733d34a110362d
fangsinan <15650701817@163.com>
2021-05-12 16:47:46 +0800

    no message

--- App/Lib/Crontab/Task.php
+++ App/Lib/Crontab/Task.php
@@ -456,9 +456,9 @@ class Task extends \EasySwoole\EasySwoole\Swoole\Task\AbstractAsyncTask
         $goodsObj = new Goods();
         $liveObj = new Live();
         foreach($push_info as $key=>$val){
-            //push_type 产品type  1专栏 2精品课 3商品 4 经营能量 5 一代天骄 6 演说能量
+            //push_type 产品type  1专栏 2精品课 3商品 4 经营能量 5 一代天骄 6 演说能量 7:讲座 8:听书  9直播   10 直播外链  11 训练营
             //push_gid 推送产品id，专栏id  精品课id  商品id
-            if(($val['push_type'] == 1 or $val['push_type'] == 7) && !empty($val['push_gid']) ){
+            if(($val['push_type'] == 1 or $val['push_type'] == 7 or $val['push_type'] == 11) && !empty($val['push_gid']) ){
                 $fields = 'id,name,price,subtitle,details_pic img,user_id';
                 $Info = $colObj->getOne($colObj->tableName,['id'=>$val['push_gid'],'status'=>1],$fields);
             }elseif(($val['push_type'] == 2 or $val['push_type'] == 8) && !empty($val['push_gid']) ){
