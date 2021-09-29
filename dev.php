d8d5d4c8a77f36604ac65f5e598bd61e3e464974
fangsinan <15650701817@163.com>
2021-09-29 11:19:09 +0800

    no message

--- dev.php
+++ dev.php
@@ -7,17 +7,17 @@
  */
 
 return [
-    'SERVER_NAME'=>"live_swoole",
+    'SERVER_NAME'=>"live_swoole_v4",
     'MAIN_SERVER'=>[
         'LISTEN_ADDRESS'=>'0.0.0.0',
-        'PORT'=>9520,
+        'PORT'=>9581,
         'SERVER_TYPE'=>EASYSWOOLE_WEB_SOCKET_SERVER, //可选为 EASYSWOOLE_SERVER  EASYSWOOLE_WEB_SERVER EASYSWOOLE_WEB_SOCKET_SERVER
         'SOCK_TYPE'=>SWOOLE_TCP, //该配置项当为SERVER_TYPE值为TYPE_SERVER时有效
         'RUN_MODEL'=>SWOOLE_PROCESS,//默认Server的运行模式
         'SETTING'=>[ //https://wiki.swoole.com/wiki/page/274.html
-            'worker_num'=>8,//运行的  worker进程数量
+            'worker_num'=>32,//运行的  worker进程数量
             'max_request'=>5000,// worker 完成该数量的请求后将退出，防止内存溢出
-            'task_worker_num'=>8,//运行的 task_worker 进程数量
+            'task_worker_num'=>32,//运行的 task_worker 进程数量
             'task_max_request'=>1000,// task_worker 完成该数量的请求后将退出，防止内存溢出
             'task_enable_coroutine' => true, //开启后自动在onTask回调中创建协程
             'reload_async' => true,//设置异步重启开关。设置为true时，将启用异步安全重启特性，Worker进程会等待异步事件完成后再退出。
@@ -48,31 +48,32 @@ return [
         'BACKLOG'     => 256,//数据队列缓冲区大小
     ],
 
-    //内网
 //    'MYSQL'=>[
-//        'host'          => 'rm-2ze0owc97gckfqz8qko.mysql.rds.aliyuncs.com',
+//        'host'          => 'pc-2ze52g145e143cs50.rwlb.rds.aliyuncs.com',
+//        #'host'          => 'eoy62ktxmkua664nxx9c-rw4rm.rwlb.rds.aliyuncs.com',
 //        'port'          => '3306',
-//        'user'          => 'bj_nlsg3',
+//        'user'          => 'bj_nlsg_v4',
 //        'timeout'       => '5',
-//        'charset'       => 'utf8mb4',
-//        'password'      => 'Rds_&0331$NLSG^v3@',
-//        'database'      => 'nlsg_v3',
+//        'charset'       => 'utf8mb4',//utf8mb4
+//        'password'      => 'Rds_&0331$NLSG^v3@_V4',
+//        'database'      => 'nlsg_v4',
 //        'POOL_MAX_NUM'  => '20',
 //        'POOL_TIME_OUT' => '0.1',
 //    ],
-    'MYSQL'=>[
-        'host'          => '39.105.214.152',
-        'port'          => '3306',
-        'user'          => 'bj_root',
-        'timeout'       => '5',
-        'charset'       => 'utf8',
-        'password'      => 'NLSG_2019cs*beijin.0410BJ',
-        'database'      => 'nlsg_v4',
-        'POOL_MAX_NUM'  => '20',
-        'POOL_TIME_OUT' => '0.1',
-    ],
 
-    //能量时光也用的这个 方便获取直播人数
+
+'MYSQL'=>[
+    'host'          => '39.105.214.152',
+    'port'          => '3306',
+    'user'          => 'bj_root',
+    'timeout'       => '5',
+    'charset'       => 'utf8',
+    'password'      => 'NLSG_2019cs*beijin.0410BJ',
+    'database'      => 'nlsg_v4',
+    'POOL_MAX_NUM'  => '20',
+    'POOL_TIME_OUT' => '0.1',
+],
+
     'REDIS'=>[
         'host'          => '39.105.214.152',
         'port'          => '6379',
@@ -81,5 +82,4 @@ return [
         'POOL_MIN_NUM'  => '5',
         'POOL_TIME_OUT' => '0.1',
     ],
-
 ];
