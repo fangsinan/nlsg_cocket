3c4604b368ca4f82c377ce843bd03c69b74f064f
zouxuguang <>
2021-11-26 15:21:57 +0800

    no message

--- App/HttpController/V1/Push.php
+++ App/HttpController/V1/Push.php
@@ -208,6 +208,11 @@ class Push extends Controller
         $UserServiceObj = new UserService();
         $UserInfo = $UserServiceObj->GetUserInfo($message['live_id'],$message['user_id']+0,$message['content'],$message['accessUserToken'],$lupInfo['user_id']);
 
+        $str_num=strlen($message['content']);
+        if($str_num>2500){
+            return ;
+        }
+        
         $admin_arr=explode('-',$lupInfo['helper']);
         if( $lupInfo['is_forb'] == 1 && !in_array($UserInfo['result']['username'],$admin_arr)){ //仅管理员评论
             return ;
