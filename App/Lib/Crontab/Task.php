101d534e565810885582fce5fb2579b8a93b788f
fangsinan <15650701817@163.com>
2022-04-14 10:24:28 +0800

    no message

--- App/Lib/Crontab/Task.php
+++ App/Lib/Crontab/Task.php
@@ -652,15 +652,11 @@ class Task extends \EasySwoole\EasySwoole\Swoole\Task\AbstractAsyncTask
                 'son_info' => $Info,
             ];
         }
-print_r($val);
-print_r($Info);
+
         //修改标记
         $LivePushObj=new LivePush();
-        $qqq = $LivePushObj->update($LivePushObj->tableName,['is_done'=>1,'done_at'=>date('Y-m-d H:i:ss',time())],['id'=>$val['id']]);
-        print_r('---');
-        print_r(['is_done'=>1,'done_at'=>date('Y-m-d H:i:ss',time())]);
-        print_r(['id'=>$val['id']]);
-        print_r('======');
+        $LivePushObj->update($LivePushObj->tableName,['is_done'=>1,'done_at'=>date('Y-m-d H:i:s',time())],['id'=>$val['id']]);
+
         return $res;
     }
 
